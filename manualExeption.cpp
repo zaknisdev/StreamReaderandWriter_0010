77ab1deccc7706b6d16a99ec175bcc41348ceccf
zaknisdev <zakianisf1710@gmail.com>
2025-06-09 10:30:47 +0700

    membuat try

--- manualExeption.cpp
+++ manualExeption.cpp
@@ -1,2 +1,11 @@
 #include <iostream>
 using namespace std;
+
+int main() 
+{
+    try {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        throw 0.5;
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+    }
+}
